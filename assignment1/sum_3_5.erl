2cc91ee8dafe8210f0815bd5d6ffece66926bad8
own-star <taras.honcharuk@gmail.com>
2016-08-26 15:37:09 +0300

    Change First

--- assignment1/sum_3_5.erl
+++ assignment1/sum_3_5.erl
@@ -4,16 +4,16 @@
 -export([find_sum/1]).
 
 find_sum(N) ->
-	find_sum(N-1,0).
+	find_sum(N - 1, 0).
 
-find_sum(2,Acc) ->
-	Acc;
-find_sum(N,Acc) ->
+find_sum(3, Acc) ->
+	Acc + 3;
+find_sum(N, Acc) ->
 	case N rem 3 of
-		0 -> find_sum(N-1,Acc+N);
+		0 -> find_sum(N-1, Acc + N);
 		_ ->
 			case N rem 5 of
-				0 -> find_sum(N-1,Acc+N);
-				_ -> find_sum(N-1,Acc)
+				0 -> find_sum(N - 1, Acc + N);
+				_ -> find_sum(N - 1, Acc)
 			end
 	end.
