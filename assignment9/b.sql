4a5f0c62dadda3d802e34a4ef7b556bf527d8bcf
own-star <taras.honcharuk@gmail.com>
2016-08-30 18:03:19 +0300

    change assignment9: fix patch update

--- assignment9/b.sql
+++ assignment9/b.sql
@@ -1,1 +1,1 @@
-update product b, assignment a set current_owner_id = (select participant_id from assignment a where a.product_id=b.id and action='m' order by ts desc limit 1);
+update product b, assignment a set b.current_owner_id = (select participant_id from assignment a where a.product_id=b.id and action='m' order by ts desc limit 1);
