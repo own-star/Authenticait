551f069ae9953df75aa044cf29c9a331fee42e37
own-star <taras.honcharuk@gmail.com>
2016-09-12 11:28:12 +0300

    Fix assignment 7: improve State

--- assignment7/collatz/src/collatz_handler.erl
+++ assignment7/collatz/src/collatz_handler.erl
@@ -13,6 +13,8 @@ stop() ->
 	gen_server:call(?MODULE, stop).
 
 
+collatz(1) -> 
+	gen_server:call(?MODULE, 1);
 collatz(N) -> 
 	gen_server:cast(?MODULE, N).
 
@@ -21,11 +23,12 @@ init(Max) ->
 	io:format("Handler init: Max: ~p here~n",[Max]),
 	timer:start(),
 	{ok, _TRef} = timer:apply_after(1000, lists,map,[fun(X) -> collatz_sup:init(X) end, lists:reverse(lists:seq(1,Max))]),
-	{ok, {Max-1, 0, {0,0}}}.
+	{ok, {0, {0,0}}}.
 
-%handle_call(_, _From, {X, X, Max} = State) ->
-%	io:format("Max: ~p~n", [Max]),
-%	{stop, normal, Max, State};
+handle_call(1, _From, {_, Max} = State) ->
+	io:format("Max: ~p~n", [Max]),
+	{stop, normal, Max, State};
+
 %handle_call({New, Count}, _From, {X, Acc, {Old, Max}}) ->
 %		State = 
 %	   		case Count > Max of
@@ -38,20 +41,20 @@ init(Max) ->
 handle_call(stop, _From, State) ->
 	{stop, normal, stopped, State}.
 
-handle_cast(_,  {X, X, Max} = State) ->
-	io:format("Max: ~p~n", [Max]),
-	{stop, normal, State};
-handle_cast(N, {X, Acc, {Old, Max}}) ->
+%handle_cast(_,  {X, X, Max} = State) ->
+%	io:format("Max: ~p~n", [Max]),
+%	{stop, normal, State};
+handle_cast(N, {Acc, {Old, Max}}) ->
 		{New, Count} = collatz_mod:calc(N),
 		State = 
 	   		case Count > Max of
-				true -> {X, Acc + 1, {New, Count}};
-				false -> {X, Acc + 1, {Old, Max}}
+				true -> {Acc + 1, {New, Count}};
+				false -> {Acc + 1, {Old, Max}}
 			end,
-%			io:format("State: ~p~n",[State]),
-			{noreply, State};
+		{noreply, State};
 
 handle_cast(_Msg, State) -> {noreply, State}.
+
 
 handle_info(_Info, State) -> {noreply, State}.
 terminate(_Reason, _State) -> ok.
